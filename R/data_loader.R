698a0d84a1463528b61f2e1a536e2cc39e45a95e
nitayalon <nitay.alon@gmail.com>
2020-05-08 09:10:14 +0300

    changing code based on latest update

--- R/data_loader.R
+++ R/data_loader.R
@@ -1,13 +1,13 @@
 library(dplyr)
 library(tidyverse)
 usa_confirmed_cases <- read_csv("./csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv")
+global_confirmed_cases <- read_csv("./csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
 usa_confirmed_cases %>% 
   str()
 usa_confirmed_cases %>% 
   select(-iso2,-iso3,-Admin2, -UID, -code3, -FIPS, -Country_Region, -Lat, -Long_, -Combined_Key) %>% 
   group_by(Province_State) %>% 
-  View()
-  # pivot_longer(-Province_State ,names_to = 'Deaths', values_to = 'count') %>% 
-  # pivot_wider(names_from = Province_State, values_from = count, values_fn = list(count = list)) %>% 
-  # select(Arizona)
+  filter(Province_State == 'New York') %>% 
+  summarise_all(list(total = sum))
   
+# Belgium, Spain, France, UK, Italy  
